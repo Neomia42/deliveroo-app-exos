750b4217a3d6abd65a8e06b171a3c07c01fe243d
Nicolas Morais <157594165+Neomia42@users.noreply.github.com>
2025-08-26 20:18:56 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+// import components
+
+import DataRest from "./components/data/DataRest";
+
+// import css
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      {/* <Header /> */}
+      <DataRest />
+      <div className="app-container"></div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/data/DataRest.jsx
@@ -0,0 +1,58 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+// import RestoInfos from "../components/RestoInfos";
+import CategoriesMenu from "../CategoriesMenu";
+import { Audio } from "react-loader-spinner";
+import Header from "../Header";
+
+const DataRest = () => {
+  const [data, setData] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(
+        "https://site--backend-deliveroo--jmytcnlt7m5p.code.run/"
+      );
+      //   console.log(response.data);
+      setData(response.data);
+      
+      // Attendre au minimum 2 secondes avant de cacher le loader
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 2000);
+    };
+
+    fetchData();
+  }, []);
+
+  return isLoading ? (
+    <div className="loader-container">
+      <Audio
+        height="30"
+        width="80"
+        radius="9"
+        color="#00BFFF"
+        ariaLabel="loading"
+        wrapperStyle
+        wrapperClass
+      />
+      {""}
+      <p>En cours de chargement...</p>
+    </div>
+  ) : (
+    <>
+      {/* {console.log("Data:", data)} */}
+
+      <Header
+        name={data.restaurant.name}
+        description={data.restaurant.description}
+        img={data.restaurant.picture}
+      />
+
+      <CategoriesMenu categories={data.categories} />
+    </>
+  );
+};
+
+export default DataRest;
